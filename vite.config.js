cfb6e410a2b2077ed0e771892f19256a427cc4b7
Abdelhamid Saidi <saidiabdelhamid.office@gmail.com>
2025-07-31 17:12:23 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/Huiz/', 
+  base: '/', 
   plugins: [vue()],
   resolve: {
     alias: {
